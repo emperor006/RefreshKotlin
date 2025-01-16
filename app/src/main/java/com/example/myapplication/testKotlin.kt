f310b4c99e4b9a02607adf26588bb533bb733d4e
emperor006 <mauricecharles1000@gmail.com>
2025-01-16 04:47:11 +0000

    deleted old code, learning numbers

--- app/src/main/java/com/example/myapplication/testKotlin.kt
+++ app/src/main/java/com/example/myapplication/testKotlin.kt
@@ -6,13 +6,6 @@ class testKotlin {
 }
 
 fun main(args:Array<String>){
-val number:Int=10
-    //Val is constants in kotlin. Its value is not changeable
-    //:Float is the data type
-    val numberF:Float=2.1383743F
 
-    print(number)
-
-    var  variableNumber: Int =99
 
 }
